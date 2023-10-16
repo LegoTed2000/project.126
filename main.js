b1a88f7030e7438d837639ae6ffc8140b4e96472
LegoTed2000 <112373501+LegoTed2000@users.noreply.github.com>
2023-10-16 17:06:51 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 petter_pan = "";
 harry_potter = "";
+leftWristx = 0;
+leftWristy = 0;
+rightWristx = 0;
+rightWristy = 0;
 
 function preload() {
     petter_pan = loadSound("music2.mp3");
@@ -12,6 +16,9 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, gotResuts);
+    pose.on("pose", gotposses)
 }
 
 function draw() {
@@ -20,4 +27,23 @@ function draw() {
 
 function sound() {
     petter_pan.play();
+    petter_pan.rate(1);
+    petter_pan.setVolume(1);
+}
+
+function gotResuts() {
+    console.log("Do you copy");
+}
+
+function gotposses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristx = results[0].pose.leftWrist.x;
+        leftWristy = results[0].pose.leftWrist.y;
+        console.log("left wrist x is - "+ leftWristx + "left wrist y is - "+ leftWristy);
+
+        rightWristx = results[0].pose.rightWrist.x;
+        rightWristy = results[0].pose.rightWrist.xy;
+        console.log("right wrist x is - "+ rightWristx + "right wrist y is - "+ rightWristy);
+    }
 }
